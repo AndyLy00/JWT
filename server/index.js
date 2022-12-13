fc07d476284d309444b723b4e1e3bf533eeaca6c
AndyLy00 <andreia.cristina.siretanu@gmail.com>
2022-12-13 13:19:52 +0200

    JWT code

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const {connect: connect1} = require('mongoose');
+const router = require('./router/index')
+
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+
+const start = async () => {
+    try {
+        mongoose.set('strictQuery', true);
+        await connect1(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+start().then()
